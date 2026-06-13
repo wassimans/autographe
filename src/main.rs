d9526c4b526099989844063a5bd4d24ec12a0a6d
wassimans <wassim@wassimans.com>
2026-06-13 19:54:28 +0200

    First editor draft using Iced text_editor widget

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,51 @@
-fn main() {
-    println!("Hello, world!");
+// pub use cosmic_text;
+
+use iced::{
+    widget::{container, text_editor},
+    Element,
+    Length::Fill,
+    Theme,
+};
+
+fn main() -> iced::Result {
+    iced::application(Editor::new, Editor::update, Editor::view)
+        .theme(Editor::theme)
+        .run()
+}
+
+struct Editor {
+    content: text_editor::Content,
+}
+
+#[derive(Debug, Clone)]
+enum Message {
+    Edit(text_editor::Action),
+}
+
+impl Editor {
+    fn new() -> Self {
+        Self {
+            content: text_editor::Content::new(),
+        }
+    }
+
+    fn update(&mut self, message: Message) {
+        match message {
+            Message::Edit(action) => {
+                self.content.perform(action);
+            }
+        }
+    }
+
+    fn view(&self) -> Element<'_, Message> {
+        let input = text_editor(&self.content)
+            .on_action(Message::Edit)
+            .height(Fill);
+
+        container(input).padding(10).into()
+    }
+
+    fn theme(&self) -> Theme {
+        Theme::Dark
+    }
 }
